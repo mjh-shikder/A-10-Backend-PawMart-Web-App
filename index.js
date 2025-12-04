485bf8824c54ded15258b1fa679e2230d356d7ce
mjh-shikder <mjhsikder@gmail.com>
2025-12-04 20:06:51 +0600

    Necessary package installed and get and listner added

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config;
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`users server started on port: ${port}`);
+    
+})
